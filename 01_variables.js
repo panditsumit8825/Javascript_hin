00428cbccc8c5e4c86ecfd0c5d143547f386fd91
sumit kumar <panditsumit8825@gmail.com>
2026-02-12 16:35:08 +0530

    changes

--- 01_variables.js
+++ 01_variables.js
@@ -2,7 +2,16 @@ const accountId = 144453
 let accountEmail ="kumar@gmail.com"
 var accountPassword="657432"
 accountCity="Bangalore"
+let accountState;
 
-// accountId= 2
+// accountId= 2 not allowed
+accountEmail="goa@gmail.com"
+accountPassword="7256268"
+accountCity="goa"
+// console.log(accountId);
 
-console.log(accountId);
+/*
+preffered to not use var in javascript
+because the issue of block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
