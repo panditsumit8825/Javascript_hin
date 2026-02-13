230dbf654a305d93c535a573e169392ee8b5d1c9
sumit kumar <panditsumit8825@gmail.com>
2026-02-13 18:50:09 +0530

    changes

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+// "use strict" ; // treat all js code as new version
+
+// // alert("hello")
+// // alert(3+3)  we are using nodejs , not browser
+
+// // let name = "kumar" // string datatype
+// // let age = 22 // number datatype
+// // let isLoggedIn = false // boolean datatype   
+// let age = 22
+// let isLoggedIn = false
+
+
+// conversion operations 
+
+let score = "88"
+console.log(typeof score)
+console.log(typeof (score));
+
+let valueInNumber = Number
+console.log();
