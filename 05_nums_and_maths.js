c4d8126f99ae974617bc4f28122ef79ede88d03a
sumit kumar <panditsumit8825@gmail.com>
2026-02-14 15:39:24 +0530

    maths concept

---
+++ 05_nums_and_maths.js
@@ -0,0 +1,17 @@
+// const score = 400
+// console.log(score);
+// const newScore = new Number(400)
+// console.log(newScore);
+
+//  ************** Maths ***************
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.2))
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min +1)) + min);
