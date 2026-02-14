94290f4953aed236d12be521dbdaa200d9ddb46c
sumit kumar <panditsumit8825@gmail.com>
2026-02-14 16:02:56 +0530

    date and time

---
+++ 06_dates.js
@@ -0,0 +1,19 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate)
+
+// let myCreateDate = new Date(2026,8,18)
+// console.log(myCreateDate);
+// console.log(myCreateDate.toLocaleDateString());
+// console.log(myCreateDate.toLocaleString());
+
+let myCreateDate = new Date("2026-05-17")
+console.log(myCreateDate);
+let mynewCreateDate = new Date("05-11-2026")
+console.log(mynewCreateDate.toLocaleString());
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-console.log(Math.random());
-// console.log((Math.random()*10) + 1)
+toLocaleString
