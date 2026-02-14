3901fa6f8d1c1b1b14e94dcb8b53aec235729141
sumit kumar <panditsumit8825@gmail.com>
2026-02-14 14:16:59 +0530

    strings

---
+++ 04_string.js
@@ -0,0 +1,17 @@
+const name = "Sumit"
+const repoCount = 16
+// console.log(name + repoCount + " Good"); this is not a good way to add string 
+
+console.log(`My name is ${name} and My repo count is ${repoCount}`);
+
+const gameName = new String('kumarpndt')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(6));
+console.log(gameName.indexOf('p'));
+
+
+
+
