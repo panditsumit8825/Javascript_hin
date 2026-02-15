99200a587fba7d6f928388a1fd0bf4c818eea506
sumit kumar <panditsumit8825@gmail.com>
2026-02-15 16:21:53 +0530

    array join

--- 07_array.js
+++ 07_array.js
@@ -12,12 +12,36 @@ const myArray = [1,3,4,5,6,7,9]
 // console.log(typeof newArray)
 
 // ***************** Slice or Splice ******************
-console.log("A",myArray);
-const myn1 = myArray.slice(1,3)
-console.log(myn1);
-console.log("B",myArray);
+// console.log("A",myArray);
+// const myn1 = myArray.slice(1,3)
+// console.log(myn1);
+// console.log("B",myArray);
 
-const myn2 = myArray.splice(1,3)
-console.log("C",myArray);
-console.log(myn2);
+// const myn2 = myArray.splice(1,3)
+// console.log("C",myArray);
+// console.log(myn2);
 
+const marvel_Heroes = [ 'thor' , "ironman" , "spiderman"]
+const dc = [ "mumbai" ,'chennai', 'gujrat']
+// marvel_Heroes.push(dc)
+// console.log(marvel_Heroes);
+console.log(marvel_Heroes.concat(dc))
+const all = [...dc,...marvel_Heroes]
+console.log(all);
+
+const anotherArray = [1,2,3,[4,5,6],7,8,[9,5,[2,4,3]]]
+console.log(anotherArray)
+const newanotherArray = anotherArray.flat(Infinity)
+console.log(newanotherArray)
+
+
+
+console.log(Array.isArray("Sumit"))
+console.log(Array.from("Sumit"))
+console.log(Array.from({name : "Sumit"}))   //Interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
